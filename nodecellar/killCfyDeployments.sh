a5b576aa3c7cb9e16224bad74bf30e305c6cddfc
Tamir Korem <tamir@gigaspaces.com>
2014-11-21 08:58:43 +0200

    added apps

---
+++ nodecellar/killCfyDeployments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+# Cancel all started executions  
+cfy deployments list | grep tamir | awk -F\| '{print $2}' | sed 's/ //g' | xargs -I file cfy executions list -d file | grep install | grep -v uninstall | grep started |  awk -F\| '{print $2}' | sed 's/ //g' |  xargs -I file cfy executions cancel -e file -f
+
+# Uninstall all apps
+cfy deployments list | grep tamir | awk -F\| '{print $2}' | sed 's/ //g' | xargs -I file cfy executions start -d file -f -w uninstall
+
+# Delete all deployments
+cfy deployments list | grep tamir | awk -F\| '{print $2}' | sed 's/ //g' | xargs -I file cfy deployments delete -f -d file
+
+# Delete all blueprints
+cfy blueprints list | grep ec2 | awk -F\| '{print $2}' | sed 's/ //g' | xargs -I file cfy blueprints delete -b file &
+
+
+
+
+
+
