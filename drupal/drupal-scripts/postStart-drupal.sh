381e7fce61b181a736b4e58670b4f9fbb9f80f43
Tamir Korem <tamir@gigaspaces.com>
2014-11-25 17:35:26 +0200

    Update postStart-drupal.sh

--- drupal/drupal-scripts/postStart-drupal.sh
+++ drupal/drupal-scripts/postStart-drupal.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 currHostName=`hostname`
-echo "test 3333 adam "
 export date1=$(date +"%s")
 documentRoot=$(ctx source node properties docRoot)
 ctx logger info "${currHostName}:$0 :documentRoot ${documentRoot}"
