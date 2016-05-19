123ec6395b69450dc5bb7c887e8e33c9b92d2fc8
tamirko <tamirkorem@gmail.com>
2016-05-19 12:56:37 +0300

    mysql

--- mysql/mysql-scripts/start-mysql.sh
+++ mysql/mysql-scripts/start-mysql.sh
@@ -20,15 +20,21 @@ sudo service mysql start || error_exit $? "Failed on: sudo service mysql start"
 
 ps -ef | grep -i mysql | grep -ivE "cfy|cloudify|grep"
 
-host_id2=$(ctx instance host_id)
+#host_id2=$(ctx instance host_id)
+#ctx logger info "aaxx host_id2 is ${host_id2}"
+
 host_id3=$(ctx instance runtime_properties host_id)
-host_id4=$(ctx _node_instance host_id)
+ctx logger info "aaxx3 host_id3 is ${host_id3}"
+
+#host_id4=$(ctx _node_instance host_id)
+
 host_id=$(ctx instance _node_instance host_id)
-ctx logger info "aaa host_id is ${host_id}"
-mngr_ip_addr=`sudo find / -name "*.log" | grep -E "${host_id}/work/${host_id}" | xargs -I file grep -E "Connecting to.*5672" file | tail -1 | xargs -I file echo file | sed -e "s+\(.*\)\(Connecting to \)\(.*\)\(:5672\)+\3+g"`
-ctx logger info "aaa mngr_ip_addr is ${mngr_ip_addr}"
+ctx logger info "aaxx3 host_id is ${host_id}"
 
-mngr_ip_addr2=`sudo grep MANAGEMENT_IP /etc/default/celeryd-${host_id} | awk -F"=" '{ print $2 }'`
-ctx logger info "aaa mngr_ip_addr2 is ${mngr_ip_addr2}"
 
+
+#mngr_ip_addr=`sudo find / -name "*.log" | grep -E "${host_id}/work/${host_id}" | xargs -I file grep -E "Connecting to.*5672" file | tail -1 | xargs -I file echo file | sed -e "s+\(.*\)\(Connecting to \)\(.*\)\(:5672\)+\3+g"`
+#ctx logger info "aaa mngr_ip_addr is ${mngr_ip_addr}"
+#mngr_ip_addr2=`sudo grep MANAGEMENT_IP /etc/default/celeryd-${host_id} | awk -F"=" '{ print $2 }'`
+#ctx logger info "aaa mngr_ip_addr2 is ${mngr_ip_addr2}"
 ctx logger info "aaa MANAGEMENT_IP is ${MANAGEMENT_IP}"
