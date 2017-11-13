3dfc36dbca89bae6cf3b88f33b393a8c0d87fa1e
tamirko <tamir@gigaspaces.com>
2017-11-13 14:22:12 +0200

    nov 13 v5

--- generic_proxy/setBlueprintValues.sh
+++ generic_proxy/setBlueprintValues.sh
@@ -8,7 +8,7 @@
 export folderPrefix=yourBlueprintName_2_
 
 # Replace the value with: Primary device's name
-export PRIMARY_NAME=FIREWALL_71
+export PRIMARY_NAME=FIREWALL_85
 # Replace the value with: Primary device's type
 export PRIMARY_TYPE=FIREWALL
 
@@ -19,12 +19,12 @@ export PRIMARY_DEVICE_ELEMENT2_NAME=ANTI_SPAM_03
 
 
 # Replace the value with: Secondary device's name
-export SECONDARY_NAME=ROUTER_892
+export SECONDARY_NAME=ROUTER_265
 # Replace the value with: Secondary device's type
 export SECONDARY_TYPE=ROUTER
 
 # Replace the value with: Secondary device's 1st element's name
-export SECONDARY_DEVICE_ELEMENT1_NAME=ROUTING_TABLE_06
+export SECONDARY_DEVICE_ELEMENT1_NAME=ROUTING_TABLE_08
 # Replace the value with: Secondary device's 2nd element's name
-export SECONDARY_DEVICE_ELEMENT2_NAME=ROUTING_TABLE_19
+export SECONDARY_DEVICE_ELEMENT2_NAME=ROUTING_TABLE_09
 
